81fdd29f712b0881b30824f46f7101523387e86f
Omkar Chandorkar <gotenksIN@aosip.dev>
2022-03-02 20:44:01 +0900

    guacamoleb: remove call for updateble apex
    * Included in partner_modules

--- device.mk
+++ device.mk
@@ -3,8 +3,6 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
